9a5cfa40ec7af176b6b819f777e73adc4581f83a
chenhongjie@qibindaojia.com <chj760515805>
2022-10-12 17:57:35 +0800

    json工具优化

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,25 @@ import { createPinia } from 'pinia';
 import { createHead } from '@vueuse/head';
 // eslint-disable-next-line import/no-unresolved
 import { registerSW } from 'virtual:pwa-register';
+import Editor from 'bin-editor-next';
+import * as ace from 'brace';
 import { plausible } from './plugins/plausible.plugin';
 import i18n from './i18n'; //引入国际化
-
-registerSW();
-
+import 'brace/ext/emmet';
+import 'brace/ext/language_tools';
+import 'brace/mode/json';
+import 'brace/snippets/json';
+import 'brace/theme/chrome';
 import { naive } from './plugins/naive.plugin';
-
 import App from './App.vue'; // 导入App组件
 import router from './router';
-
+registerSW();
 const app = createApp(App); // 创建App实例 传入渲染组件
-
 app.use(createPinia());
 app.use(createHead());
 app.use(router);
 app.use(naive);
 app.use(plausible);
 app.use(i18n); // 挂载国际化配置
-
+app.component(Editor.name, Editor);
 app.mount('#app');
--- src/tools/json-viewer/json-viewer.vue
+++ src/tools/json-viewer/json-viewer.vue
@@ -1,52 +1,90 @@
 <template>
-  <n-form-item
-    label="Your raw json"
-    :feedback="rawJsonValidation.message"
-    :validation-status="rawJsonValidation.status"
-  >
-    <n-input
-      ref="inputElement"
-      v-model:value="rawJson"
-      placeholder="Paste your raw json here..."
-      type="textarea"
-      rows="20"
-      autocomplete="off"
-      autocorrect="off"
-      autocapitalize="off"
-      spellcheck="false"
-    />
-  </n-form-item>
-  <n-form-item label="Prettify version of your json">
-    <textarea-copyable :value="cleanJson" language="json" :follow-height-of="inputElement" />
-  </n-form-item>
+  <n-card>
+    <n-form-item label="Your raw json">
+      <n-input
+        ref="inputElement"
+        v-model:value="rawJson"
+        placeholder="Paste your raw json here..."
+        type="textarea"
+        rows="20"
+        autocomplete="off"
+        autocorrect="off"
+        autocapitalize="off"
+        spellcheck="false"
+      />
+    </n-form-item>
+  </n-card>
+  <n-card>
+    <n-form-item label="Your raw json">
+      <b-ace-editor
+        v-model="jsonStr"
+        lang="json"
+        width="100%"
+        height="500"
+        theme="xcode"
+        :readonly="readonly"
+        :font-size="fontSize"
+      ></b-ace-editor>
+    </n-form-item>
+  </n-card>
+
+  <n-col span="12">
+    <div class="pl-15">
+      <div class="pl-15">
+        <n-space>
+          <n-button size="small" type="primary" @click="handleFormat">格式化</n-button>
+          <n-button size="small" type="primary" @click="modal = true">弹窗编辑</n-button>
+          <n-button size="small" type="primary" @click="handleZip">压缩</n-button>
+
+          <n-modal v-model="modal" title="弹窗编辑" transition-name="fade-in-linear">
+            <b-ace-editor v-model="jsonStr" height="400"></b-ace-editor>
+          </n-modal>
+        </n-space>
+      </div>
+    </div>
+  </n-col>
 </template>
 
-<script setup lang="ts">
-import TextareaCopyable from '@/components/TextareaCopyable.vue';
-import { useValidation } from '@/composable/validation';
-import { withDefaultOnError } from '@/utils/defaults';
-import JSON5 from 'json5';
-import { computed, ref } from 'vue';
-
-const inputElement = ref<HTMLElement>();
-
-const rawJson = ref('{"hello": "world"}');
-const cleanJson = computed(() => withDefaultOnError(() => JSON.stringify(JSON5.parse(rawJson.value), null, 3), ''));
-
-const rawJsonValidation = useValidation({
-  source: rawJson,
-  rules: [
+<script>
+const jsonData = {
+  title: '测试json数据',
+  child: [
     {
-      validator: (v) => v === '' || JSON5.parse(v),
-      message: 'Provided JSON is not valid.',
+      title: '子项名称1',
+      desc: '子项描述1',
+    },
+    {
+      title: '子项名称2',
+      desc: '子项描述2',
     },
   ],
-});
+};
+export default {
+  data() {
+    return {
+      jsonStr: JSON.stringify(jsonData, null, 2),
+      rawJson: '',
+      modal: false,
+      readonly: false,
+      theme: 'chrome',
+      fontSize: 14,
+    };
+  },
+  methods: {
+    handleZip() {
+      this.jsonStr = JSON.stringify(JSON.parse(this.rawJson), null, 0);
+    },
+    handleFormat() {
+      this.jsonStr = JSON.stringify(JSON.parse(this.rawJson), null, 2);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .result-card {
   position: relative;
+
   .copy-button {
     position: absolute;
     top: 10px;
